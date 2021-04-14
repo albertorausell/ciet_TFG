da1314d59b1f91c064fd36aebb20c373167f0167
DESKTOP-OFLCBVJ\rause <alberto.rausell@ara-tech.es>
2021-04-14 19:19:16 +0200

    CREATE until organization, and profiles

--- learner_app/models.py
+++ learner_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class LearnerProfile (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    rol = models.CharField(max_length=20)
+    imagen = models.ImageField(upload_to='img/users')
+    points = models.IntegerField()
+
+    class Meta:
+        verbose_name = 'learner'
+        verbose_name_plural = 'learners'
+
+  #  def __str__(self):
+  #      return user.get_full_name(self)
