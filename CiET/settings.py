e4595dcb2b0f3b5e3ee34f1bb0eb7827f61e390b
DESKTOP-OFLCBVJ\rause <alberto.rausell@ara-tech.es>
2021-08-03 12:44:32 +0200

    a

--- CiET/settings.py
+++ CiET/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'common_app/static')
 STATCIFILES_DIR = (os.path.join(BASE_DIR, 'common_app/static'), )
 
 MEDIA_URL = '/media/'
